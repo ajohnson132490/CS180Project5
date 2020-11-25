430eb37c69043a585c8a5d004a852b76b24f7374
jacob-zietek <xoqcpn@gmail.com>
2020-11-25 03:44:04 -0500

    Initial server commit.

---
+++ src/Server.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * BetterBook Server
+ *
+ * This is the server designed to send, store, and
+ * process data from clients.
+ *
+ * @author Jacob Zietek | CS18000 Project 5 | Group 007-2
+ * @version 25 November 2020
+ */
+
+public class Server implements Runnable {
+
+    public Server(){
+
+    }
+
+    public void run() {
+
+    }
+
+}
